14aa4980ab09feed77aa2573d88bbad607d37fd9
ernest opoku-kwarteng <eopokukwarteng@gmail.com>
2026-03-05 00:35:46 +0000

    Update settings.py

--- farm_management/settings.py
+++ farm_management/settings.py
@@ -257,7 +257,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'farm_management.wsgi.application'
-
+ASGI_APPLICATION = 'farm_management.asgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
